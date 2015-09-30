13e65d22fc422bab2f005d0b56af8fac18c1bd4f
Guilherme Garnier <guilherme.garnier@gmail.com>
2015-09-29 20:18:51 -0500

    Add Yahoo Weather interface

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -16,6 +17,7 @@ func main() {
 	mw := multiWeatherProvider{
 		openWeatherMap{},
 		weatherUnderground{apiKey: *wundergroundAPIKey},
+		yahooWeather{},
 	}
 
 	http.HandleFunc("/weather/", func(w http.ResponseWriter, r *http.Request) {
@@ -129,4 +131,43 @@ func (w weatherUnderground) temperature(city string) (float64, error) {
 	kelvin := d.Observation.Celsius + 273.15
 	log.Printf("weatherUnderground: %s: %.2f", city, kelvin)
 	return kelvin, nil
+}
+
+type yahooWeather struct{}
+
+func (w yahooWeather) temperature(city string) (float64, error) {
+	resp, err := http.Get("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22" + city + "%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys")
+	if err != nil {
+		return 0, err
+	}
+
+	defer resp.Body.Close()
+
+	var d struct {
+		Query struct {
+			Results struct {
+				Channel struct {
+					Item struct {
+						Condition struct {
+							Temp string `json:"temp"`
+						} `json:"condition"`
+					} `json:"item"`
+				} `json:"channel"`
+			} `json:"results"`
+		} `json:"query"`
+	}
+
+	if err := json.NewDecoder(resp.Body).Decode(&d); err != nil {
+		return 0, err
+	}
+
+	tempFarenheit, err := strconv.ParseFloat(d.Query.Results.Channel.Item.Condition.Temp, 64)
+	if err != nil {
+		return 0, err
+	}
+
+	tempKelvin := (tempFarenheit + 459.67) * 5 / 9
+
+	log.Printf("yahooWeather: %s: %.2f", city, tempKelvin)
+	return tempKelvin, nil
 }
